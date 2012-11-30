fd6c8d1cad4b81fdf8f2b63ca0dca66e5077287b
blegule <jciur001@fiu.edu>
2012-11-30 14:04:23 -0500

    Simplied Code
    Simplified the declaration of variables and properties to 1 line,
    making it easier to read.

--- AgentBuddy/AgentBuddy/CPCCustomerInfo.h
+++ AgentBuddy/AgentBuddy/CPCCustomerInfo.h
@@ -11,58 +11,17 @@
 
 @interface CPCCustomerInfo : NSObject
 {
-    NSString *customerNumber;
-    NSString *firstName;
-    NSString *lastName;
-    NSString *houseAddress;
-    NSString *city;
-    NSString *state;
-    NSString *zipCode;
-    NSString *email;
-    NSString *phoneNumber;
-    NSString *birthDate;
-    NSString *licenseNumber;
+    NSString *customerNumber, *firstName, *lastName, *houseAddress, *city, *state, *zipCode, *email, *phoneNumber, *birthDate, *licenseNumber;
 
     sqlite3 *database;
     
-    NSMutableArray *customerNumberArray;
-    NSMutableArray *firstNameArray;
-    NSMutableArray *lastNameArray;
-    NSMutableArray *addressArray;
-    NSMutableArray *cityArray;
-    NSMutableArray *stateArray;
-    NSMutableArray *zipCodeArray;
-    NSMutableArray *emailArray;
-    NSMutableArray *phoneNumberArray;
-    NSMutableArray *birthDateArray;
-    NSMutableArray *licenseNumberArray;
-
-    
+    NSMutableArray *customerNumberArray, *firstNameArray, *lastNameArray, *addressArray, *cityArray, *stateArray, *zipCodeArray, *emailArray, *phoneNumberArray, *birthDateArray, *licenseNumberArray;
+   
 }
 
-@property (nonatomic,retain)NSString *customerNumber;
-@property (nonatomic,retain)NSString *firstName;
-@property (nonatomic,retain)NSString *lastName;
-@property (nonatomic,retain)NSString *houseAddress;
-@property (nonatomic,retain)NSString *city;
-@property (nonatomic,retain)NSString *state;
-@property (nonatomic,retain)NSString *zipCode; 
-@property (nonatomic,retain)NSString *email;
-@property (nonatomic,retain) NSString *phoneNumber;
-@property (nonatomic,retain)NSString *birthDate;
-@property (nonatomic,retain)NSString *licenseNumber;
+@property (nonatomic,retain)NSString *customerNumber, *firstName, *lastName, *houseAddress, *city, *state, *zipCode, *email, *phoneNumber, *birthDate, *licenseNumber;
 
-@property (nonatomic,retain) NSMutableArray *customerNumberArray;
-@property (nonatomic,retain) NSMutableArray *firstNameArray;
-@property (nonatomic, retain) NSMutableArray *lastNameArray;
-@property (nonatomic, retain) NSMutableArray *addressArray;
-@property (nonatomic, retain) NSMutableArray *cityArray;
-@property (nonatomic, retain) NSMutableArray *stateArray;
-@property (nonatomic,retain) NSMutableArray *zipCodeArray;
-@property (nonatomic, retain) NSMutableArray *emailArray;
-@property (nonatomic, retain) NSMutableArray *phoneNumberArray;
-@property (nonatomic,retain) NSMutableArray *birthDateArray;
-@property (nonatomic,retain) NSMutableArray *licenseNumberArray;
+@property (nonatomic,retain) NSMutableArray *customerNumberArray, *firstNameArray, *lastNameArray, *addressArray, *cityArray, *stateArray, *zipCodeArray, *emailArray, *phoneNumberArray, *birthDateArray, *licenseNumberArray;
 
 -(void) makeDBCopyAsNeeded;
 -(void) getCustomernInfo;

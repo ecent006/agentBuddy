0199786dbc87dc4e859c11e4c22aab8ca3841f14
rprie004 <rub1319@hotmail.com>
2012-12-01 16:05:05 -0500

    test

---
+++ AgentBuddy/AgentBuddy/CPCClaimsInfo.h
@@ -0,0 +1,13 @@
+//
+//  CPCClaimsInfo.h
+//  AgentBuddy
+//
+//  Created by Lion User on 11/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CPCClaimsInfo : NSObject
+
+@end
---
+++ AgentBuddy/AgentBuddy/CPCClaimsInfo.m
@@ -0,0 +1,13 @@
+//
+//  CPCClaimsInfo.m
+//  AgentBuddy
+//
+//  Created by Lion User on 11/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CPCClaimsInfo.h"
+
+@implementation CPCClaimsInfo
+
+@end
